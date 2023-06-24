4f0b70c9041e8d953b8cf76ea01a27476d87d95f
vats <vatsalmangukiya9003@gmail.com>
2023-06-24 09:21:42 +0530

    os module

---
+++ 45. Os Module/index.js
@@ -0,0 +1,7 @@
+const os=require('os');
+// console.log(os.arch()); //System architecture :x64
+// console.log(os.freemem()/(1024*1024*1024)); //freeram
+// console.log(os.totalmem()/(1024*1024*1024)); //total RAM
+// console.log(os.hostname()); //PC NAME
+// console.log(os.platform()); 
+// console.log(os.userInfo()); //User INFO
